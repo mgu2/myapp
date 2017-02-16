224eeab1293c942454ec05ab48748dbbd2a01d7a
gmx9308 <gmx9308@gmail.com>
2017-02-16 15:03:48 -0800

    design home page
    change home page from default style

--- src/myapp/wsgi.py
+++ src/myapp/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
