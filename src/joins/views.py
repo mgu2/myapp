1f444a867e4fe9736646b78ddc8b75ffba830d67
gmx9308 <gmx9308@gmail.com>
2017-02-17 00:48:39 -0800

    add reference code

--- src/joins/views.py
+++ src/joins/views.py
@@ -7,7 +7,7 @@ from .models import Join
 
 from .forms import EmailForm
 from .models import Join
-
+import uuid
 
 def get_ip(request):
     try:
@@ -21,7 +21,16 @@ def get_ip(request):
     return ip
 
 
-
+def get_ref_id():
+    ref_id = str(uuid.uuid4())[:11].replace('-','').lower()
+    
+    try:
+        id_exists = Join.objects.get(ref_id=ref_id)
+        get_ref_id()
+    except:
+        return ref_id
+    
+    
 def home(request):
     form = JoinForm(request.POST or None)  
     if form.is_valid():
@@ -29,6 +38,7 @@ def home(request):
         email = form.cleaned_data['email']
         new_join_old, created = Join.objects.get_or_create(email=email)
         if created:
+            new_join_old.ref_id = get_ref_id()
             new_join_old.ip_address = get_ip(request)
             new_join_old.save()
         #redirect
