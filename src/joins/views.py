7e04073add5843829a85bcfa9531c7e08c765e40
gmx9308 <gmx9308@gmail.com>
2017-02-16 20:53:57 -0800

    add email subscription

--- src/joins/views.py
+++ src/joins/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .forms import EmailForm, JoinForm
+from .models import Join
 
+from .forms import EmailForm
+from .models import Join
 def home(request):
-    context = {}
+    form = EmailForm(request.POST or None)  
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        new_join, created = Join.objects.get_or_create(email=email)
+
+#    form = JoinForm(request.POST or None)
+#    if form.is_valid():
+#        new_join = form.save(commit=False)
+#        new_join.save()
+#        
+    context = { "form": form } 
     template = "home.html"
     return render(request, template, context)
