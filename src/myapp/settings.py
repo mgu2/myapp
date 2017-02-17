6b99ccfc48a86346106a0b22e208d3450e918535
gmx9308 <gmx9308@gmail.com>
2017-02-16 23:18:57 -0800

    covert to south
    convert db to south and add new fields

--- src/myapp/settings.py
+++ src/myapp/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'joins'
+    'joins',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
